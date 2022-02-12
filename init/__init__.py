83bda518eea9ed140fa0a8f0ee6964ff0a23746e
Olli Suominen <79847649+olli-suominen@users.noreply.github.com>
2022-02-12 10:36:40 +0200

    Create __init__.py

---
+++ init/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""The initialization module to add the submodules to the python path."""
+
+import os
+import sys
+
+SUB_MODULES = [
+    "domain-messages",
+    "domain-messages/simulation-tools"
+]
+
+for sub_module in SUB_MODULES:
+    library_path = os.path.realpath(sub_module)
+    if library_path not in sys.path:
+        sys.path.append(library_path)
